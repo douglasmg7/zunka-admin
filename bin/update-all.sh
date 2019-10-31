fcbc1f88b6e67de329e4b00e1ea4153a53ba7401
douglasmg7 <douglasmg7@gmail.com>
2019-10-31 16:43:29 -0300

    update system script

---
+++ bin/update-all.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Go source variable must be defined.
+[[ -z "$GS" ]] && printf "error: GS - Go Source enviorment variable not defined.\n" >&2 && exit 1 
+
+echo :: Checking bluetang repository...
+cd $GS/bluetang
+RES=`git pull`
+if [[ ! $RES == Already* ]]; then 
+    INSTALL_ZUNKASRV=true
+    echo bluetang repository updated.
+fi
+
+echo :: Checking currency repository...
+cd $GS/currency
+RES=`git pull`
+if [[ ! $RES == Already* ]]; then
+    INSTALL_ZUNKASRV=true
+    INSTALL_ALDOWSC=true
+    echo currency repository updated.
+fi
+
+echo :: Checking aldoutil repository...
+cd $GS/aldoutil
+RES=`git pull`
+if [[ ! $RES == Already* ]]; then
+    INSTALL_ZUNKASRV=true
+    INSTALL_ALDOWSC=true
+    echo aldoutil repository updated.
+fi
+
+echo :: Checking aldowsc repository...
+cd $GS/aldowsc
+RES=`git pull`
+if [[ ! $RES == Already* ]]; then
+    INSTALL_ALDOWSC=true
+    echo aldowsc repository updated.
+fi
+
+echo :: Checking zunkasrv repository...
+cd $GS/zunkasrv
+RES=`git pull`
+if [[ ! $RES == Already* ]]; then
+    INSTALL_ZUNKASRV=true
+    echo zunkasrv repository updated.
+fi
+
+# Install aldowsc.
+if [[ $INSTALL_ALDOWSC == true ]]; then
+    echo :: Installing aldowsc...
+    cd $GS/aldowsc
+    go install
+fi
+
+# Install zunkasrv.
+if [[ $INSTALL_ZUNKASRV == true ]]; then
+    echo :: Installing zunkasrv...
+    cd $GS/zunkasrv
+    go install
+fi
